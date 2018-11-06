341a96c415c242db85f7ab0b0ade6f5ac09947d5
inderjeet0007 <inderjeet.saluja97@gmail.com>
2018-11-06 16:15:42 +0530

    del

--- header.php
+++
@@ -1,9 +0,0 @@
-<div id="page-wrapper">
-    <div class="row">
-        <div class="col-lg-12">
-            <h1><i class="fa fa-dashboard"></i> <?php echo $main_title ?></h1>
-            <hr>
-
-        </div>
-    </div><!-- /.row -->
-<div class="row">
